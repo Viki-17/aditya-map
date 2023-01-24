8912958d7321e319848ee4bde34ebf978b88e6a8
Sudhashankar KJ <sudhashankar.kj@gmail.com>
2023-01-25 00:04:11 +0530

    fix: layout issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavbarV2";
+import Tagline from "./components/Tagline";
 import { MyComponent } from "./MyComponent";
 
 function App() {
   return (
-    <>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        margin: 'auto'
+      }}
+    >
       <Navbar />
+      <Tagline
+        text='There are quite a few 3rd party libraries to choose from and for this article I went with React-Socks.'
+      />
+      <Banner
+        text='Find an Aditya Birla Capital branch near you'
+      />
       <MyComponent />
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,75 @@
 import "./Footer.css";
 
+const FooterMenu = ({ children }) => {
+  return (
+    <span
+      style={{
+        fontSize: '15px',
+        cursor: 'pointer',
+        userSelect: 'none'
+      }}
+    >
+      {children}
+    </span>
+  )
+}
+
 const Footer = () => {
   return (
-    <>
-      <div className="footer">
-        <h1>ADITYA BRILA CAPITAL</h1>
+    <div
+      style={{
+        width: '100%',
+        backgroundColor: '#6c7174',
+        height: '200px',
+        color: '#fff',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        columnGap: '150px',
+        marginTop: '100px'
+      }}
+    >
+      <img
+        src='https://www.adityabirlacapital.com/-/media/Project/ABCL/Logo/footer-logo.png'
+        style={{ height: '30px' }}
+      />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'start',
+          rowGap: '10px'
+        }}
+      >
+        <FooterMenu>About Us</FooterMenu>
+        <FooterMenu>Careers</FooterMenu>
+        <FooterMenu>Customer Service</FooterMenu>
       </div>
-    </>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'start',
+          rowGap: '10px'
+        }}
+      >
+        <FooterMenu>Our Solutions</FooterMenu>
+        <FooterMenu>Investor Relations</FooterMenu>
+        <FooterMenu>Press and Media</FooterMenu>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'start',
+          rowGap: '10px'
+        }}
+      >
+        <FooterMenu>Our Business</FooterMenu>
+        <FooterMenu>Our Achievements</FooterMenu>
+        <FooterMenu>Locate Us</FooterMenu>
+      </div>
+    </div>
   );
 };
 
